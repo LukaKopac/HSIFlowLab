ba32500ce3740215c66c32d40addc2255f694b7c
Luka Kopac <lukako.bf@gmail.com>
2026-09-23 20:49:37 +0200

    Functioning SavitzkyGolay smoothing, preparation for derivatives

---
+++ SpectrumKit.Tests/SavitzkyGolayTests.cs
@@ -0,0 +1,66 @@
+﻿using HSIDataKit.Models;
+using SpectrumKit.Processing;
+
+namespace SpectrumKit.Tests;
+
+public class SavitzkyGolayTests
+{
+    [Fact]
+    public void Smooth_ConstantSpectrum_RemainsConstant()
+    {
+        var spectrum = new Spectrum(
+            new double[] { 1, 2, 3, 4, 5, 6, 7 },
+            new double[] { 5, 5, 5, 5, 5, 5, 5 },
+            "Test");
+
+        var result = SavitzkyGolay.Smooth(
+            spectrum,
+            windowSize: 5,
+            polynomialOrder: 2);
+
+        foreach (double value in result.Values)
+        {
+            Assert.Equal(5, value, 10);
+        }
+    }
+
+    [Fact]
+    public void Smooth_LinearSpectrum_RemainsLinear()
+    {
+        var spectrum = new Spectrum(
+            new double[] { 1, 2, 3, 4, 5, 6, 7 },
+            new double[] { 1, 2, 3, 4, 5, 6, 7 },
+            "Test");
+
+        var result = SavitzkyGolay.Smooth(
+            spectrum,
+            windowSize: 5,
+            polynomialOrder: 2);
+
+        for (int i = 0; i < result.Values.Length; i++)
+        {
+            Assert.Equal(
+                spectrum.Values[i],
+                result.Values[i],
+                10);
+        }
+    }
+
+    [Fact]
+    public void Smooth_ReducesLocalSpike()
+    {
+        var spectrum = new Spectrum(
+            new double[] { 1, 2, 3, 4, 5, 6, 7 },
+            new double[] { 1, 2, 10, 4, 5, 6, 7 },
+            "Test");
+
+        var result = SavitzkyGolay.Smooth(
+            spectrum,
+            windowSize: 5,
+            polynomialOrder: 2);
+
+        Assert.True(result.Values[2] < 10);
+    }
+
+
+}
--- SpectrumKit/Processing/SavitzkyGolay.cs
+++ SpectrumKit/Processing/SavitzkyGolay.cs
@@ -32,16 +32,76 @@ public static class SavitzkyGolay
                 nameof(polynomialOrder));
         }
 
+        int halfWindow = windowSize / 2;
+        
+        double[] coefficients =
+            CalculateCoefficients(
+                windowSize,
+                polynomialOrder,
+                derivativeOrder: 0);
 
+        double[] smoothedValues =
+            new double[spectrum.Values.Length];
+
+        // Temporarily preserve the edge values
+        Array.Copy(
+            spectrum.Values,
+            smoothedValues,
+            spectrum.Values.Length);
+
+        for (int i = halfWindow; i < spectrum.Values.Length - halfWindow; i++)
+        {
+            double value = 0;
+
+            for (int j = 0; j < windowSize; j++)
+            {
+                int sourceIndex =
+                    i - halfWindow + j;
+
+                value +=
+                    coefficients[j] *
+                    spectrum.Values[sourceIndex];
+            }
+
+            smoothedValues[i] = value;
+        }
+
+        SmoothEdges(
+            spectrum,
+            smoothedValues,
+            windowSize,
+            polynomialOrder);
+
+        return new Spectrum(
+            spectrum.Wavelengths,
+            smoothedValues,
+            spectrum.Name);
     }
 
     private static double[] CalculateCoefficients(
-    int windowSize,
-    int polynomialOrder)
+        int windowSize,
+        int polynomialOrder,
+        int derivativeOrder)
     {
+        if (derivativeOrder < 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(derivativeOrder),
+                "derivativeOrder must be non-negative.");
+        }
+
+        if (derivativeOrder > polynomialOrder)
+        {
+            throw new ArgumentException(
+                "derivativeOrder must not be greater than polynomialOrder.",
+                nameof(derivativeOrder));
+        }
+        
         int halfWindow = windowSize / 2;
 
-        var matrix = new double[windowSize, polynomialOrder + 1];
+        var designMatrix = new Matrix(
+            windowSize,
+            polynomialOrder + 1);
 
         for (int row = 0; row < windowSize; row++)
         {
@@ -49,37 +109,154 @@ public static class SavitzkyGolay
 
             for (int column = 0; column <= polynomialOrder; column++)
             {
-                matrix[row, column] = Math.Pow(x, column);
+                designMatrix[row, column] =
+                    Math.Pow(x, column);
             }
         }
 
-        var ata = new double[polynomialOrder + 1, polynomialOrder + 1];
+        Matrix transpose = designMatrix.Transpose();
 
-        for (int row = 0; row <= polynomialOrder; row++)
-        {
-            for (int column = 0; column <= polynomialOrder; column++)
-            {
-                for (int i = 0; i < windowSize; i++)
-                {
-                    ata[row, column] +=
-                        matrix[i, row] * matrix[i, column];
-                }
-            }
-        }
+        Matrix normalMatrix =
+            transpose.Multiply(designMatrix);
 
-        var ataInverse = InvertMatrix(ata);
+        Matrix inverse =
+            normalMatrix.Inverse();
+
+        Matrix coefficientsMatrix =
+            inverse.Multiply(transpose);
 
         var coefficients = new double[windowSize];
 
+        double derivativeFactor = Factorial(derivativeOrder);
+
         for (int i = 0; i < windowSize; i++)
         {
-            for (int j = 0; j <= polynomialOrder; j++)
-            {
-                coefficients[i] +=
-                    ataInverse[0, j] * matrix[i, j];
-            }
+            coefficients[i] =
+                derivativeFactor *
+                coefficientsMatrix[derivativeOrder, i];
         }
 
         return coefficients;
+    }
+
+    private static double[] CalculateEdgeCoefficients(
+        int windowSize,
+        int polynomialOrder,
+        double x)
+    {
+        var designMatrix =
+            new Matrix(windowSize, polynomialOrder + 1);
+
+        for (int row = 0; row < windowSize; row++)
+        {
+            for (int column = 0; column <= polynomialOrder; column++)
+            {
+                designMatrix[row, column] =
+                    Math.Pow(row, column);
+            }
+        }
+
+        Matrix transpose =
+            designMatrix.Transpose();
+
+        Matrix normalMatrix = 
+            transpose.Multiply(designMatrix);
+
+        Matrix inverse = 
+            normalMatrix.Inverse();
+
+        Matrix mapping = 
+            inverse.Multiply(transpose);
+
+        var coefficients =
+            new double[windowSize];
+
+        for (int i = 0; i < windowSize; i++)
+        {
+            double coefficient = 0;
+
+            for (int j = 0; j <= polynomialOrder; j++)
+            {
+                coefficient +=
+                    Math.Pow(x, j) *
+                    mapping[j, i];
+            }
+
+            coefficients[i] = coefficient;
+        }
+
+        return coefficients;
+    }
+
+    private static void SmoothEdges(
+        Spectrum spectrum,
+        double[] smoothedValues,
+        int windowSize,
+        int polynomialOrder)
+    {
+        int halfWindow = windowSize / 2;
+
+        // Left edge
+        for (int i = 0; i < halfWindow; i++)
+        {
+            double[] coefficients =
+                CalculateEdgeCoefficients(
+                    windowSize,
+                    polynomialOrder,
+                    i);
+
+            double value = 0;
+
+            for (int j = 0; j < windowSize; j++)
+            {
+                value +=
+                    coefficients[j] *
+                    spectrum.Values[j];
+            }
+
+            smoothedValues[i] = value;
+        }
+
+        // Right edge
+        for (int i = 0; i < halfWindow; i++)
+        {
+            int outputIndex =
+                spectrum.Values.Length - halfWindow + i;
+
+            double x =
+                windowSize - halfWindow + i;
+
+            double[] coefficients =
+                CalculateEdgeCoefficients(
+                    windowSize,
+                    polynomialOrder,
+                    x);
+
+            double value = 0;
+
+            int startIndex =
+                spectrum.Values.Length - windowSize;
+
+            for (int j = 0; j < windowSize; j++)
+            {
+                value +=
+                    coefficients[j] *
+                    spectrum.Values[startIndex + j];
+            }
+
+            smoothedValues[outputIndex] = value;
+        }
+    }
+
+    private static int Factorial(int value)
+    {
+        int result = 1;
+
+        for (int i = 2; i <= value; i++)
+        {
+            result *= i;
+        }
+
+        return result;
     }
 }
