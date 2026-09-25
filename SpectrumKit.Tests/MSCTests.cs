4db7e86cc43f54c3b91f040424da7ff9c4aabe03
Luka Kopac <lukako.bf@gmail.com>
2026-09-25 14:19:57 +0200

    Implemented MSC preprocessing and corresponding tests

---
+++ SpectrumKit.Tests/MSCTests.cs
@@ -0,0 +1,177 @@
+﻿using HSIDataKit.Models;
+using SpectrumKit.Processing;
+
+namespace SpectrumKit.Tests;
+
+public class MSCTests
+{
+    [Fact]
+    public void Apply_IdenticalSpectrum_ReturnsUnchangedSpectrum()
+    {
+        double[] wavelengths =
+        {
+        100, 110, 120, 130, 140
+    };
+
+        double[] values =
+        {
+        1, 2, 3, 4, 5
+    };
+
+        var spectrum =
+            new Spectrum(
+                wavelengths,
+                values,
+                "test");
+
+        var reference =
+            new Spectrum(
+                wavelengths,
+                values,
+                "reference");
+
+        Spectrum result =
+            MSC.Apply(
+                spectrum,
+                reference);
+
+        Assert.Equal(
+            values,
+            result.Values);
+    }
+
+    [Fact]
+    public void Apply_CorrectsOffsetAndScaling()
+    {
+        double[] wavelengths = {100, 110, 120, 130, 140};
+
+        double[] referenceValues = {1, 2, 3, 4, 5};
+
+        double[] spectrumValues = {3, 5, 7, 9, 11};
+
+        var spectrum =
+            new Spectrum(
+                wavelengths,
+                spectrumValues);
+
+        var reference =
+            new Spectrum(
+                wavelengths,
+                referenceValues);
+
+        Spectrum result =
+            MSC.Apply(
+                spectrum,
+                reference);
+
+        for (int i = 0; i < referenceValues.Length; i++)
+        {
+            Assert.Equal(
+                referenceValues[i],
+                result.Values[i],
+                precision: 10);
+        }
+    }
+
+    [Fact]
+    public void Apply_RejectsConstantReference()
+    {
+        double[] wavelengths = { 100, 110, 120, 130, 140 };
+
+        double[] referenceValues = { 1, 1, 1, 1, 1 };
+
+        double[] spectrumValues = { 2, 3, 4, 5, 6 };
+
+        var spectrum =
+            new Spectrum(
+                wavelengths,
+                spectrumValues);
+
+        var reference =
+            new Spectrum(
+                wavelengths,
+                referenceValues);
+
+        Assert.Throws<ArgumentException>(() =>
+            MSC.Apply(
+                spectrum,
+                reference));
+    }
+
+    [Fact]
+    public void Apply_RejectsDifferentWavelengthCount()
+    {
+        double[] spectrumWavelengths = { 100, 110, 120, 130, 140 };
+        double[] referenceWavelengths = { 100, 110, 120, 130 };
+
+        var spectrum =
+            new Spectrum(
+                spectrumWavelengths,
+                new double[] { 1, 2, 3, 4, 5 });
+
+        var reference =
+            new Spectrum(
+                referenceWavelengths,
+                new double[] { 1, 2, 3, 4 });
+
+        Assert.Throws<ArgumentException>(() =>
+            MSC.Apply(
+                spectrum,
+                reference));
+    }
+
+    [Fact]
+    public void Apply_RejectsDifferentWavelengths()
+    {
+        double[] spectrumWavelengths = { 100, 110, 120, 130, 140 };
+        double[] referenceWavelengths = { 100, 110, 120, 135, 140 };
+
+        var spectrum =
+            new Spectrum(
+                spectrumWavelengths,
+                new double[] { 1, 2, 3, 4, 5 });
+
+        var reference =
+            new Spectrum(
+                referenceWavelengths,
+                new double[] { 1, 2, 3, 4, 5 });
+
+        Assert.Throws<ArgumentException>(() =>
+            MSC.Apply(
+                spectrum,
+                reference));
+    }
+
+    [Fact]
+    public void Apply_CorrectsOffsetOnly()
+    {
+        double[] wavelengths = { 100, 110, 120, 130, 140 };
+
+        double[] referenceValues = { 1, 2, 3, 4, 5 };
+
+        double[] spectrumValues = { 3, 4, 5, 6, 7 };
+
+        var spectrum =
+            new Spectrum(
+                wavelengths,
+                spectrumValues);
+
+        var reference =
+            new Spectrum(
+                wavelengths,
+                referenceValues);
+
+        Spectrum result =
+            MSC.Apply(
+                spectrum,
+                reference);
+
+        for (int i = 0; i < referenceValues.Length; i++)
+        {
+            Assert.Equal(
+                referenceValues[i],
+                result.Values[i],
+                precision: 10);
+        }
+    }
+}
---
+++ SpectrumKit/Processing/MSC.cs
@@ -0,0 +1,74 @@
+﻿using HSIDataKit.Models;
+
+namespace SpectrumKit.Processing;
+
+public static class MSC
+{
+    public static Spectrum Apply(
+        Spectrum spectrum,
+        Spectrum reference)
+    {
+        ArgumentNullException.ThrowIfNull(spectrum);
+        ArgumentNullException.ThrowIfNull(reference);
+
+        if (spectrum.Wavelengths.Length != reference.Wavelengths.Length)
+        {
+            throw new ArgumentException(
+                "Spectrum and reference must have the same number of wavelengths.",
+                nameof(reference));
+        }
+
+        for (int i = 0; i < spectrum.Wavelengths.Length; i++)
+        {
+            if (spectrum.Wavelengths[i] != reference.Wavelengths[i])
+            {
+                throw new ArgumentException(
+                    "Spectrum and reference must have matching wavelengths.",
+                    nameof(reference));
+            }
+        }
+
+        double referenceMean = reference.Values.Average();
+
+        double spectrumMean = spectrum.Values.Average();
+
+        double covariance = 0;
+        double referenceVariance = 0;
+
+        for (int i = 0; i < spectrum.Values.Length; i++)
+        {
+            double referenceDifference =
+                reference.Values[i] - referenceMean;
+
+            double spectrumDifference = 
+                spectrum.Values[i] - spectrumMean;
+
+            covariance += referenceDifference * spectrumDifference;
+
+            referenceVariance += referenceDifference * referenceDifference;
+        }
+
+        if (referenceVariance == 0)
+        {
+            throw new ArgumentException(
+                "Reference spectrum must not be constant.",
+                nameof(reference));
+        }
+
+        double slope = covariance / referenceVariance;
+        double intercept = spectrumMean - slope * referenceMean;
+
+        double[] correctedValues = new double[spectrum.Values.Length];
+
+        for (int i = 0; i < spectrum.Values.Length; i++)
+        {
+            correctedValues[i] =
+                (spectrum.Values[i] - intercept) / slope;
+        }
+
+        return new Spectrum(
+            spectrum.Wavelengths,
+            correctedValues,
+            spectrum.Name);
+    }
+}
